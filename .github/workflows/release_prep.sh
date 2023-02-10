4c72d472ebfe97aa674966aeaa8e3511ef6555a4
Alex Eagle <alex@aspect.dev>
2023-02-10 06:28:04 -0800

    release: publish release artifact
    GitHub's stability guarantee for the archive is iffy, and we want metrics on downloads.
    
    See https://github.com/bazel-contrib/SIG-rules-authors/issues/11#issuecomment-1411222578

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -5,8 +5,11 @@ set -o errexit -o nounset -o pipefail
 # Set by GH actions, see
 # https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables
 TAG=${GITHUB_REF_NAME}
+# The prefix is chosen to match what GitHub generates for source archives
 PREFIX="rules_oci-${TAG:1}"
-SHA=$(git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip | shasum -a 256 | awk '{print $1}')
+ARCHIVE="rules_oci-$TAG.tar.gz"
+git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip > $ARCHIVE
+SHA=$(shasum -a 256 $ARCHIVE | awk '{print $1}')
 
 cat << EOF
 WORKSPACE snippet:
@@ -16,7 +19,7 @@ http_archive(
     name = "contrib_rules_oci",
     sha256 = "${SHA}",
     strip_prefix = "${PREFIX}",
-    url = "https://github.com/bazel-contrib/rules_oci/archive/${TAG}.tar.gz",
+    url = "https://github.com/bazel-contrib/rules_oci/releases/download/${TAG}/${ARCHIVE}",
 )
 
 # Fetches the rules_oci dependencies.
