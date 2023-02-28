adc996db459de18159b66e45d8917cfdf5e3fe4e
thesayyn <thesayyn@gmail.com>
2023-02-28 17:39:30 +0300

    add external test for attaching

---
+++ examples/attach_external/test.bash
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -o pipefail -o errexit -o nounset
+
+export HOME="$TEST_TMPDIR"
+
+readonly COSIGN="${1/external\//../}"
+readonly CRANE="${2/external\//../}"
+readonly REGISTRY_LAUNCHER="${3/external\//../}"
+readonly ATTACHER="$4"
+readonly IMAGE_PATH="$5"
+readonly SBOM_PATH="$6"
+
+
+# Launch a registry instance at a random port
+source "${REGISTRY_LAUNCHER}"
+REGISTRY=$(start_registry $TEST_TMPDIR $TEST_TMPDIR/output.log)
+echo "Registry is running at ${REGISTRY}"
+
+readonly REPOSITORY="${REGISTRY}/local" 
+
+# attach the sbom
+"${ATTACHER}" --repository "${REPOSITORY}"
+
+# due to https://github.com/sigstore/cosign/issues/2603 push the image 
+REF=$(mktemp)
+"${CRANE}" push "${IMAGE_PATH}" "${REPOSITORY}" --image-refs="${REF}"
+
+# download the sbom
+"${COSIGN}" download sbom $(cat $REF) > "$TEST_TMPDIR/download.sbom"
+
+diff -u --ignore-space-change --strip-trailing-cr "$SBOM_PATH"  "$TEST_TMPDIR/download.sbom" || (echo "FAIL: downloaded SBOM does not match the original" && exit 1)
