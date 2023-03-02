ad56df1eecc6df51e240c97707484e9cc0c3531f
thesayyn <thesayyn@gmail.com>
2023-03-02 19:37:26 +0300

    ci: update release workflow

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -13,22 +13,18 @@ SHA=$(shasum -a 256 $ARCHIVE | awk '{print $1}')
 
 cat << EOF
 WORKSPACE snippet:
+
 \`\`\`starlark
+
 load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
+
 http_archive(
     name = "contrib_rules_oci",
     sha256 = "${SHA}",
     strip_prefix = "${PREFIX}",
     url = "https://github.com/bazel-contrib/rules_oci/releases/download/${TAG}/${ARCHIVE}",
 )
-
-# Fetches the rules_oci dependencies.
-# If you want to have a different version of some dependency,
-# you should fetch it *before* calling this.
-# Alternatively, you can skip calling this function, so long as you've
-# already fetched all the dependencies.
-load("@contrib_rules_oci//oci:dependencies.bzl", "rules_oci_dependencies")
-rules_oci_dependencies()
-
-\`\`\`
 EOF
+
+awk 'f;/--SNIP--/{f=1}' e2e/workspace/WORKSPACE
+echo "\`\`\`" 
