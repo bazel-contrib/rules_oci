ccaaf13ef3e272b17f48dcf2bfac92725562bfd6
thesayyn <thesayyn@gmail.com>
2023-04-07 00:11:11 +0300

    migrate to bats

--- e2e/auth/main.go
+++ e2e/auth/main.go
@@ -76,7 +76,7 @@ func main() {
 	reg := registry.New(registry.Logger(log.New(ioutil.Discard, "", log.LstdFlags)))
 	s := &http.Server{
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			if r.RequestURI == "/v2/distroless/static/manifests/sha256:c3c3d0230d487c0ad3a0d87ad03ee02ea2ff0b3dcce91ca06a1019e07de05f12" {
+			if r.RequestURI == "/v2/empty_image/static/manifests/sha256:c3c3d0230d487c0ad3a0d87ad03ee02ea2ff0b3dcce91ca06a1019e07de05f12" {
 				currentAuth := Authn{Authorization: []string{}}
 				if r.Header["Authorization"] != nil {
 					currentAuth.Authorization = r.Header["Authorization"]
