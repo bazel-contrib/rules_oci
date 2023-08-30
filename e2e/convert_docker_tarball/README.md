70598df0b0a3af2af412d80d0b2f23aa384ba358
Alex Eagle <alex@aspect.dev>
2023-08-30 14:55:30 -0700

    code review comments

--- e2e/convert_docker_tarball/README.md
+++ e2e/convert_docker_tarball/README.md
@@ -2,8 +2,4 @@
 
 In some cases, your legacy setup doesn't fetch a base image from a remote registry, instead you've produced your base image in a script and check or fetch the tarball.
 
-To generate it run;
-
-```bash
-docker buildx build e2e/convert_docker_tarball --output=type=docker,dest=e2e/convert_docker_tarball/image.tar --builder=cool_swirles -t test:latest
-```
+To generate the `image.tar` file, first run `create_base_image.bash`. Then build the example normally.
--- e2e/convert_docker_tarball/convert.bash
+++ e2e/convert_docker_tarball/convert.bash
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
+# Most rules_oci users will use oci_pull to fetch base layers from a remote registry.
+# However, you might build docker-format tarballs and uploaded them to Artifactory for example.
+# rules_oci expects an OCI format base image, so these need to be converted.
+#
+# This just requires push'ing the tarball into a locally-running registry which
+# understands both formats, then pull'ing back out in the oci format.
+#
+# Note, this is suboptimal because Bazel will still have to execute an action that has the entire
+# base image as an input. Large inputs cause network delays with remote execution.
+
 set -o pipefail -o errexit -o nounset
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
