f40862e6b0f327aa561f5b39be618cc40415c52e
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2024-10-25 22:39:06 +0200

    Remove workspace_status example
    It's not used and has portability issues

--- e2e/smoke/workspace_status.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# This serves as an example of how the workspace_status.sh script can be used to
-# stamp the `created` attribute of an image.
-BUILD_TIMESTAMP=${BUILD_TIMESTAMP:-$(date +%s)} # macOS specific
-BUILD_ISO8601=$(date -u -r "$BUILD_TIMESTAMP" +"%Y-%m-%dT%H:%M:%SZ")
-echo "BUILD_ISO8601 $BUILD_ISO8601"
