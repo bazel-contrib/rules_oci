d884f5840001f8c2006b9cc979d744ec72ec5003
Alex Eagle <alex@aspect.dev>
2024-12-12 10:53:37 -0800

    chore: fix tag name when called by workflow (#749)

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -2,9 +2,9 @@
 
 set -o errexit -o nounset -o pipefail
 
-# Set by GH actions, see
-# https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables
-TAG=${GITHUB_REF_NAME}
+# Argument provided by reusable workflow caller, see
+# https://github.com/bazel-contrib/.github/blob/d197a6427c5435ac22e56e33340dff912bc9334e/.github/workflows/release_ruleset.yaml#L72
+TAG=$1
 # The prefix is chosen to match what GitHub generates for source archives
 PREFIX="rules_oci-${TAG:1}"
 ARCHIVE="rules_oci-$TAG.tar.gz"
