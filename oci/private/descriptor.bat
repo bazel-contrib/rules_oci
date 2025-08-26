fff377782b98f27131897123e94c5c09b6d9fdcd
Chris Brown <77508021+peakschris@users.noreply.github.com>
2025-08-26 03:41:33 -0400

    remove hard dependency on msys2

--- oci/private/descriptor.bat
+++ oci/private/descriptor.bat
@@ -1,7 +1,7 @@
 @echo off
 SETLOCAL ENABLEEXTENSIONS
 SETLOCAL ENABLEDELAYEDEXPANSION
-for %%a in ("c:/msys64/usr/bin/bash.exe") do set "bash_bin_dir=%%~dpa"
+for %%a in (%BAZEL_SH%) do set "bash_bin_dir=%%~dpa"
 set PATH=%bash_bin_dir%;%PATH%
 set "parent_dir=%~dp0"
 set "parent_dir=!parent_dir:\=/!"
@@ -11,4 +11,4 @@ if defined args (
   set args=!args:\=\\\\!
   set args=!args:"=\"!
 )
-"c:/msys64/usr/bin/bash.exe" -c "%parent_dir%descriptor.sh !args!"
+%BAZEL_SH% -c "%parent_dir%descriptor.sh !args!"
